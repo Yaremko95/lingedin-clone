5130593b427b3efebba42e61b5991d92e93fc04c
tetiana <tetianayaremko@gmail.com>
2021-03-06 18:01:06 +0100

    adds 1on1 chat functionality

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,12 +1,34 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../../context/socket/SocketProvider";
 import ChatHeader from "../ChatHeader";
 import { AuthContext } from "../../context/auth/Auth";
+import CropOriginalIcon from "@material-ui/icons/CropOriginal";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import GifIcon from "@material-ui/icons/Gif";
+import MoodIcon from "@material-ui/icons/Mood";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import {
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  List,
+} from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+
+import { useStyles } from "./styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function Chat(props) {
   const { chat } = props;
   const { socket } = useContext(SocketContext);
   const { user: authorizedUser } = useContext(AuthContext);
+  const [text, setText] = useState("");
+  const classes = useStyles();
   useEffect(() => {
     const participants = chat.participants.map((participant) => participant.id);
     socket.emit("join", { conversationId: chat.id, participants });
@@ -14,15 +36,88 @@ function Chat(props) {
   const participant = chat.participants.find(
     (participant) => participant.user.id !== authorizedUser.id
   );
-  console.log("participant", chat);
+  console.log("messages", chat.messages);
+  const sendMessage = () => {
+    socket.emit("sendMsg", { conversationId: chat.id, text });
+    setText("");
+  };
+  const messagesEndRef = React.useRef(null);
+  //
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chat.messages]);
+  //
+  // return (
+  //     <div>
+  //       {messages.map(message => <Message key={message.id} {...message} />)}
+  //       <div ref={messagesEndRef} />
+  //     </div>
   return (
-    <div>
-      <ChatHeader
-        toggleModal={props.toggleModal}
-        show={props.show}
-        participant={participant}
-      />
-    </div>
+    <>
+      <div>
+        <ChatHeader
+          toggleModal={props.toggleModal}
+          show={props.show}
+          participant={participant}
+        />
+      </div>
+
+      <div style={{ overflowY: "auto", height: "257px" }}>
+        <List>
+          {chat.messages.map((msg) => (
+            <ListItem button>
+              <ListItemIcon>
+                <Avatar className={classes.avatar} src={msg.user.imgUrl} />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography className={classes.title}>
+                    {msg.user.fullName}
+                  </Typography>
+                }
+                secondary={
+                  <div className={classes.module}>
+                    {" "}
+                    <Typography className={classes.msg}>{msg.text}</Typography>
+                  </div>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+        <div ref={messagesEndRef} />
+      </div>
+      <div style={{ position: "absolute", bottom: "0", width: "100%" }}>
+        <TextField
+          style={{ width: "100%" }}
+          id="outlined-multiline-static"
+          value={text}
+          onChange={(e) => setText(e.currentTarget.value)}
+          multiline
+          rows={1}
+          variant="outlined"
+        />
+        <div className={classes.footer}>
+          <div className={classes.footerSectionWrapper}>
+            <CropOriginalIcon className={classes.footerIcon} />
+            <AttachFileIcon className={classes.footerIcon} />
+            <GifIcon className={classes.footerIcon} />
+            <MoodIcon className={classes.footerIcon} />
+            <VideocamIcon className={classes.footerIcon} />
+          </div>
+          <div className={classes.footerSectionWrapper}>
+            <Button variant="contained" color={"primary"} onClick={sendMessage}>
+              Send
+            </Button>
+            <MoreHorizIcon className={classes.footerIcon} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Chat/styles.js
@@ -0,0 +1,44 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(1),
+  },
+  user: {
+    padding: "0 10px",
+  },
+  avatar: {
+    height: "48px",
+    width: "48px",
+  },
+  title: {
+    color: "rgba(0,0,0,0.7)",
+    fontWeight: "600",
+    fontSize: "0.9rem",
+  },
+  module: {
+    overflow: "hidden",
+    display: "-webkit-box",
+    WebkitLineClamp: 2,
+    WebkitBoxOrient: "vertical",
+  },
+  msg: {
+    color: "rgba(0,0,0,0.5)",
+    fontSize: "0.8rem",
+  },
+  footer: {
+    // width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    padding: theme.spacing(1, 0),
+  },
+  footerSectionWrapper: {
+    display: "flex",
+  },
+  footerIcon: {
+    fontSize: "1.2rem",
+
+    color: "rgba(0, 0, 0, 0.6)",
+    margin: theme.spacing(0, 1),
+  },
+}));
